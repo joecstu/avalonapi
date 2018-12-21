6d4b8e5e729147e73c280d01c35638fccf08ef4a
pongsakon <pongsakonpromma@gmail.com>
2018-12-22 00:18:26 +0700

    เพิ่มฟังชั่นเล็กน้อย

--- main.go
+++ main.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"net/http"
+	"strings"
 
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 )
+
+type ID struct {
+	Username string
+	Password string
+}
 
 func main() {
 	// Echo instance
@@ -17,6 +23,7 @@ func main() {
 
 	// Routes
 	e.GET("/", hello)
+	e.POST("login",Login)
 
 	// Start server
 	e.Logger.Fatal(e.Start(":1323"))
@@ -24,5 +31,29 @@ func main() {
 
 // Handler
 func hello(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello, World!")
+
+	return c.JSON(http.StatusOK, "Hello, World!")
+}
+func Login(c echo.Context) error {
+
+	var request struct {
+		Username string
+		Password string
+	}
+	var response struct {
+		Status        string `json:",omitempty"` //"success | error | inactive"
+		StatusMessage string `json:",omitempty"`
+	}
+
+	c.Bind(&request)
+
+	if strings.Compare(request.Username, "test") == 0 && strings.Compare(request.Password, "1234") == 0 {
+		response.Status="เข้าสู่ระบบได้จ้า"
+		response.StatusMessage="เข้าเกมได้เลย"
+	}else{
+		response.Status="เข้าสู่ระบบไม่ได้จ้า"
+		response.StatusMessage="ไปส่องกะหรี่"
+	}
+
+	return c.JSON(http.StatusOK,response)
 }
