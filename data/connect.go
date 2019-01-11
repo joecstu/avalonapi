a01130e038badedb82a2f6194b4b07600c0254d8
pongsakon <pongsakonpromma@gmail.com>
2019-01-11 22:07:21 +0700

    ลองโน้นลองนี้

--- data/connect.go
+++ data/connect.go
@@ -2,13 +2,15 @@ package data
 
 import (
 	"crypto/tls"
-	"github.com/globalsign/mgo"
 	"net"
+
+	"github.com/globalsign/mgo"
 )
 
 const DATABASE = "mongodb://test:test1234@cluster0-shard-00-00-ikbru.gcp.mongodb.net:27017,cluster0-shard-00-01-ikbru.gcp.mongodb.net:27017,cluster0-shard-00-02-ikbru.gcp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin"
 
-func CreateSession() (*mgo.Session,error) {
+//CreateSessiion สร้างการเชื่อมต่อเป็นท่อให้
+func CreateSession() (*mgo.Session, error) {
 	var mongoURI = DATABASE
 	tlsConfig := &tls.Config{InsecureSkipVerify: true}
 	dialInfo, err := mgo.ParseURL(mongoURI)
@@ -16,5 +18,5 @@ func CreateSession() (*mgo.Session,error) {
 		return tls.Dial("tcp", addr.String(), tlsConfig)
 	}
 	session, err := mgo.DialWithInfo(dialInfo)
-	return session,err
+	return session, err
 }
